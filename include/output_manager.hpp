64fb35916787b61cd638d36a124df2b14ffcfd98
Arthur Lage <arthurlage2006@gmail.com>
2025-06-10 21:29:10 -0300

    add output manager

---
+++ include/output_manager.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <cstdio>
+#include <cstring>
+
+#include <cstdio>
+#include <cstring>
+#include <string>
+
+class OutputManager {
+public:
+    OutputManager();
+    explicit OutputManager(size_t bufferSize);
+    ~OutputManager();
+    
+    bool openFile(const char* filename);
+    bool openInOutcome(const char* filename);
+    void closeFile();
+    void write(const char* data, size_t length);
+    void flush();
+    bool isFileOpen() const;
+    size_t getBufferSize() const;
+
+private:
+    FILE* filePtr;
+    char* buffer;
+    size_t bufferSize;
+    size_t bufferPos;
+    
+    void allocateBuffer(size_t size);
+    void freeBuffer();
+};
