1ac8162ab6eb436f7d277df305822a4fa59b9adb
Arthur Lage <arthurlage2006@gmail.com>
2025-06-12 13:02:24 -0300

    super fast

--- src/main.cpp
+++ src/main.cpp
@@ -50,23 +50,50 @@ void writeExploreFile(const std::vector<int>& users, const std::string& filename
 int main() {
     auto start = chrono::high_resolution_clock::now();
 
-    InputPreprocessor ratings_file("kaggle-data/ratings.csv", "r");
+    auto ppS = chrono::high_resolution_clock::now();
 
+    InputPreprocessor ratings_file("kaggle-data/ratings.csv", "r");
     ratings_file.process_ratings();
+
+    auto ppE = chrono::high_resolution_clock::now();
+
+
+
+    auto umrS = chrono::high_resolution_clock::now();
 
     BinaryReader binary_reader("datasets/input.dat");
     UsersAndMoviesData usersAndMovies;
     binary_reader.process_input(usersAndMovies);
 
+    auto umrE = chrono::high_resolution_clock::now();
+
+
+    auto rmS = chrono::high_resolution_clock::now();
     MoviesData movies;
     MovieReader movieReader("kaggle-data/movies.csv", "r");
     movieReader.getMovies(movies);
+    auto rmE = chrono::high_resolution_clock::now();
+
+
+
+    auto recS = chrono::high_resolution_clock::now();
     
     RecommenderCosine recommenderCosine;
     recommenderCosine.generateRecommendations(usersAndMovies, movies);
 
+    auto recE = chrono::high_resolution_clock::now();
+
+
     auto progEnd = chrono::high_resolution_clock::now();
     auto dur = chrono::duration_cast<chrono::milliseconds>(progEnd - start);
+
+
+    cout << "\n====== TIMES\n";
+    cout << "Time to pre process: "<< chrono::duration_cast<chrono::milliseconds>(ppS - ppE).count() << " ms" << endl;
+    cout << "Time to read users and movies: "<< chrono::duration_cast<chrono::milliseconds>(umrS - umrE).count() << " ms" << endl;
+    cout << "Time to read movies: " << chrono::duration_cast<chrono::milliseconds>(rmS - rmE).count() << " ms" << endl;
+    cout << "Time to recommend: " << chrono::duration_cast<chrono::milliseconds>(recS - recE).count() << " ms" << endl;
+
     cout << "Total time: " << dur.count() << " ms" << endl;
 
     return 0;
