984b819ad57ac7e2bcfcbc37932f004f01740608
William D Leao <wdsbleao@gmail.com>
2025-07-01 15:46:04 -0300

    v2

---
+++ src/main.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <random>
+
+
+#include "custom_types.hpp"
+#include "input_preprocessor.hpp"
+#include "binary_reader.hpp"
+#include "movie_reader.hpp"
+#include "recommender_cosine.hpp"
+
+using namespace std;
+
+std::vector<int> selectRandomUsers(const UsersAndMoviesData& usersAndMovies, int sampleSize = 50) {
+    std::vector<int> allUsers;
+    for (const auto& [userId, _] : usersAndMovies) {
+        allUsers.push_back(userId);
+    }
+
+    sampleSize = std::min(sampleSize, static_cast<int>(allUsers.size()));
+    if (sampleSize <= 0) return {};
+
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::shuffle(allUsers.begin(), allUsers.end(), rng);
+
+    return {allUsers.begin(), allUsers.begin() + sampleSize};
+}
+
+// New function to write users to explore.dat
+void writeExploreFile(const std::vector<int>& users, const std::string& filename = "datasets/explore.dat") {
+    std::ofstream outFile(filename);
+    
+    if (!outFile.is_open()) {
+        std::cerr << "Error: Could not open file " << filename << " for writing!\n";
+        return;
+    }
+
+    for (int userId : users) {
+        outFile << userId << "\n";
+    }
+
+    outFile.close();
+    std::cout << "Successfully wrote " << users.size() << " users to " << filename << "\n";
+}
+
+int main() {
+    auto start = chrono::high_resolution_clock::now();
+
+    auto ppS = chrono::high_resolution_clock::now();
+
+    InputPreprocessor ratings_file("kaggle-data/ratings.csv", "r");
+    ratings_file.process_ratings();
+
+    auto ppE = chrono::high_resolution_clock::now();
+
+    auto umrS = chrono::high_resolution_clock::now();
+
+    BinaryReader binary_reader("datasets/input.dat");
+    UsersAndMoviesData usersAndMovies;
+    binary_reader.process_input(usersAndMovies);
+
+    // SHOW USERS AND MOVIES
+
+    // for(auto el : usersAndMovies) {
+    //     cout << "ID: " << el.first << endl;
+    //     for (auto mv : el.second) {
+    //         cout << "Movie: " << mv.movie << ", Score: " << mv.score << endl;
+    //     }
+    // }
+    
+    auto umrE = chrono::high_resolution_clock::now();
+
+    auto rmS = chrono::high_resolution_clock::now();
+    MoviesData movies;
+    MovieReader movieReader("kaggle-data/movies.csv", "r");
+    movieReader.getMovies(movies);
+
+//  for(auto el : movies) {
+//         cout << "ID: " << el.first << ", Nome: " << el.second << endl;
+        
+//     }
+
+    auto rmE = chrono::high_resolution_clock::now();
+
+
+
+    auto recS = chrono::high_resolution_clock::now();
+    
+    RecommenderCosine recommenderCosine;
+    recommenderCosine.generateRecommendations(usersAndMovies, movies);
+
+    auto recE = chrono::high_resolution_clock::now();
+
+
+    auto progEnd = chrono::high_resolution_clock::now();
+    auto dur = chrono::duration_cast<chrono::milliseconds>(progEnd - start);
+
+
+    cout << "\n====== TIMES\n";
+    cout << "Time to pre process: "<< chrono::duration_cast<chrono::milliseconds>(ppS - ppE).count() << " ms" << endl;
+    cout << "Time to read users and movies: "<< chrono::duration_cast<chrono::milliseconds>(umrS - umrE).count() << " ms" << endl;
+    cout << "Time to read movies: " << chrono::duration_cast<chrono::milliseconds>(rmS - rmE).count() << " ms" << endl;
+    cout << "Time to recommend: " << chrono::duration_cast<chrono::milliseconds>(recS - recE).count() << " ms" << endl;
+
+    cout << "Total time: " << dur.count() << " ms" << endl;
+
+    return 0;
+}
